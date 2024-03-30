5aea7297d45fb653c10185ff8c381db2cb71a6c2
Matheus Leal <matheusspleal@gmail.com>
2024-03-30 16:33:44 -0300

    feat: add env helper

---
+++ src/core/shared/config/helpers/env.ts
@@ -0,0 +1,54 @@
+import { EnvVariableNotProvidedError } from '@/core/shared/config/errors/env-variable-not-provided-error'
+import { FailedToConvertEnvVariableError } from '@/core/shared/config/errors/failed-to-convert-env-variable-error'
+import { EnvVariableEnum } from '@/core/shared/config/types/env-variable-enum'
+
+export type TypeOfValue = boolean | number | string
+
+export type EnvProps<T extends TypeOfValue> =
+  | {
+      key: string
+      type: T extends boolean
+        ? EnvVariableEnum.BOOLEAN
+        : T extends number
+          ? EnvVariableEnum.NUMBER
+          : EnvVariableEnum.STRING
+      defaultValue?: T
+    }
+  | {
+      key: string
+      type?: T extends boolean
+        ? EnvVariableEnum.BOOLEAN
+        : T extends number
+          ? EnvVariableEnum.NUMBER
+          : EnvVariableEnum.STRING
+      defaultValue: T
+    }
+
+function hasDefaultValue(defaultValue: any): boolean {
+  return !['', null, undefined].includes(defaultValue)
+}
+
+export function env<T extends TypeOfValue>({
+  key,
+  type,
+  defaultValue,
+}: EnvProps<T>): T {
+  const value = process.env[key] || defaultValue
+  const typeOfValue = type ?? String(typeof defaultValue)
+  if (!hasDefaultValue(value)) {
+    throw new EnvVariableNotProvidedError(key, typeOfValue)
+  }
+  const valueInStr = String(value)
+  try {
+    switch (typeOfValue) {
+      case EnvVariableEnum.BOOLEAN:
+        return JSON.parse(valueInStr) as T
+      case EnvVariableEnum.NUMBER:
+        return Number(valueInStr) as T
+      default:
+        return valueInStr as T
+    }
+  } catch (err) {
+    throw new FailedToConvertEnvVariableError(key, typeOfValue)
+  }
+}
