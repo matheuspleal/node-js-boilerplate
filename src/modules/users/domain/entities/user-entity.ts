12e17ef23413ca419992b0bf6f3126256263cca8
Matheus Leal <matheusspleal@gmail.com>
2024-03-30 21:32:03 -0300

    feat: add user entity

---
+++ src/modules/users/domain/entities/user-entity.ts
@@ -0,0 +1,71 @@
+import { Entity } from '@/core/domain/entities/entity'
+import { UniqueEntityId } from '@/core/domain/value-objects/unique-entity-id'
+import { Optional } from '@/core/shared/types/optional'
+import { Birthdate } from '@/modules/users/domain/value-objects/birthdate'
+import { Email } from '@/modules/users/domain/value-objects/email'
+
+export type UserProps = {
+  name: string
+  email: Email
+  birthdate: Birthdate
+  createdAt: Date
+  updatedAt: Date
+}
+
+export class UserEntity extends Entity<UserProps> {
+  get name() {
+    return this.props.name
+  }
+
+  set name(name: string) {
+    this.props.name = name
+    this.touch()
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get birthdate() {
+    return this.props.birthdate
+  }
+
+  get age() {
+    return this.props.birthdate.getCurrentAgeInYears()
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  static create(
+    props: Optional<
+      Omit<UserProps, 'email' | 'birthdate'>,
+      'createdAt' | 'updatedAt'
+    > & {
+      email: string
+      birthdate: Date
+    },
+    id?: UniqueEntityId,
+  ): UserEntity {
+    const user = new UserEntity(
+      {
+        ...props,
+        birthdate: new Birthdate(new Date(props.birthdate)),
+        email: new Email(props.email),
+        createdAt: props.createdAt ?? new Date(),
+        updatedAt: props.updatedAt ?? new Date(),
+      },
+      id,
+    )
+    return user
+  }
+}
