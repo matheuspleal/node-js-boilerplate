dcfeb666762b7cab56bb4da07b604e194b90e886
Matheus Leal <matheusspleal@gmail.com>
2024-03-30 21:45:31 -0300

    feat: add get user by id use case

---
+++ src/modules/users/application/errors/user-not-found-error.ts
@@ -0,0 +1,8 @@
+import { UseCaseError } from '@/core/application/use-cases/errors/use-case-error'
+
+export class UserNotFoundError extends Error implements UseCaseError {
+  constructor(id: string) {
+    super(`User with id "${id}" not found`)
+    this.name = 'UserNotFoundError'
+  }
+}
