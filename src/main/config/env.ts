c9e9c231236269e17b649e0ade99a1107cebed79
Matheus Leal <matheusspleal@gmail.com>
2024-04-21 16:34:16 -0300

    chore: update env variables

---
+++ src/main/config/env.ts
@@ -0,0 +1,19 @@
+import { env } from '@/core/shared/config/helpers/env'
+
+export const server = {
+  host: env<string>({ key: 'HOST', defaultValue: 'localhost' }),
+  port: env<number>({ key: 'PORT', defaultValue: 3333 }),
+}
+
+export const api = {
+  currentVersion: env<string>({ key: 'VERSION', defaultValue: 'v1' }),
+}
+
+export const database = {
+  host: env<string>({ key: 'DB_HOST', defaultValue: 'localhost' }),
+  port: env<number>({ key: 'DB_PORT', defaultValue: 5432 }),
+  name: env<string>({ key: 'DB_NAME', defaultValue: 'node_js_skeleton' }),
+  user: env<string>({ key: 'DB_USER', defaultValue: 'john_doe' }),
+  schema: env<string>({ key: 'DB_SCHEMA', defaultValue: 'public' }),
+  password: env<string>({ key: 'DB_PASSWORD', defaultValue: 'doe_john' }),
+}
