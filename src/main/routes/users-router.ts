972484d322d5dac3a2f145dea744e4d5ff23aaaf
Matheus Leal <matheusspleal@gmail.com>
2024-04-25 14:32:53 -0300

    feat: add new enpoint in users router

--- src/main/routes/users-router.ts
+++ src/main/routes/users-router.ts
@@ -3,8 +3,10 @@ import { type FastifyInstance } from 'fastify'
 import { fastifyRouterAdapter } from '@/main/adapters/fastify-router-adapter'
 import { makeCreateUserController } from '@/main/factories/users/presentation/controllers/create-user-controller-factory'
 import { makeFetchUsersController } from '@/main/factories/users/presentation/controllers/fetch-users-controller-factory'
+import { makeGetUserByIdController } from '@/main/factories/users/presentation/controllers/get-user-by-id-controller-factory'
 import { type CreateUser } from '@/modules/users/presentation/controllers/create-user-controller'
 import { type FetchUsers } from '@/modules/users/presentation/controllers/fetch-users-controller'
+import { type GetUsersById } from '@/modules/users/presentation/controllers/get-user-by-id-controller'
 
 const usersRouterPrefix = '/users'
 
@@ -13,6 +15,12 @@ export default async function userRouter(app: FastifyInstance) {
     usersRouterPrefix,
     fastifyRouterAdapter<FetchUsers.Request, FetchUsers.Response>(
       makeFetchUsersController(),
+    ),
+  )
+  app.get(
+    `${usersRouterPrefix}/:id`,
+    fastifyRouterAdapter<GetUsersById.Request, GetUsersById.Response>(
+      makeGetUserByIdController(),
     ),
   )
   app.post(
