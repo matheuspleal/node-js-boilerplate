f1d08410cc91424b64fb1988148bee0c261463fa
Matheus Leal <matheusspleal@gmail.com>
2024-05-01 10:56:36 -0300

    feat: add new helper in http helpers

--- src/core/presentation/helpers/http-helpers.ts
+++ src/core/presentation/helpers/http-helpers.ts
@@ -1,5 +1,6 @@
 import { ServerError } from '@/core/presentation/errors/server-error'
 import { type HttpResponse } from '@/core/presentation/protocols/http'
+import { type UnauthorizedError } from '@/modules/users/application/errors/unauthorized-error'
 
 export function ok<T = any>(body: T): HttpResponse<T> {
   return {
@@ -18,6 +19,15 @@ export function created<T = any>(body: T): HttpResponse<T> {
 export function badRequest(error: Error): HttpResponse<Error> {
   return {
     statusCode: 400,
+    body: error,
+  }
+}
+
+export function unauthorizedError(
+  error: UnauthorizedError,
+): HttpResponse<Error> {
+  return {
+    statusCode: 401,
     body: error,
   }
 }
