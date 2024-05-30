872d21e1a190ef0b28372818f454f0246eadeef1
Matheus Leal <matheusspleal@gmail.com>
2024-05-30 09:15:24 -0300

    feat: add presenter contract

---
+++ src/core/presentation/presenters/presenter.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export abstract class Presenter<DTO, Output> {
+  protected constructor() {}
+  /* Implement the methods as needed */
+  // abstract toJSON(dto: DTO): Output
+  // abstract toCSV(dto: DTO): string
+}
