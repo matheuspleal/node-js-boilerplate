f54ec2d913addc62664c8787264538f7d1528c8a
Matheus Leal <matheusspleal@gmail.com>
2026-01-02 10:21:36 -0300

    refactor: make InternalServerError extend PresentationError

--- src/core/presentation/errors/internal-server.error.ts
+++ src/core/presentation/errors/internal-server.error.ts
@@ -1,7 +1,8 @@
-export class InternalServerError extends Error {
+import { PresentationError } from '@/core/presentation/errors/presentation.error'
+
+export class InternalServerError extends PresentationError {
   constructor(error?: Error) {
     super('Server failed')
-    this.name = 'InternalServerError'
     this.stack = error?.stack
   }
 }
