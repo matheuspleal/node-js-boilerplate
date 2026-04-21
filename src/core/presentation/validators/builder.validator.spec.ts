17bca469123224dba45358d6b25afeac23a476ff
Matheus Leal <matheusspleal@gmail.com>
2026-04-21 18:16:05 -0300

    test(core): cover isValidDate validator scenarios

--- src/core/presentation/validators/builder.validator.spec.ts
+++ src/core/presentation/validators/builder.validator.spec.ts
@@ -1,5 +1,6 @@
 import { BuilderValidator } from '@/core/presentation/validators/builder.validator'
 import { IsEmailRule } from '@/core/presentation/validators/rules/is-email.rule'
+import { IsValidDateRule } from '@/core/presentation/validators/rules/is-valid-date.rule'
 import { IsValidPasswordRule } from '@/core/presentation/validators/rules/is-valid-password.rule'
 import { IsValidUUIDRule } from '@/core/presentation/validators/rules/is-valid-uuid.rule'
 import { RequiredRule } from '@/core/presentation/validators/rules/required.rule'
@@ -15,15 +16,18 @@ describe('BuilderValidator', () => {
       .isValidUUID()
       .isValidPassword()
       .isEmail()
+      .isValidDate()
 
     const validators = builderValidator.build()
-    const [requiredRule, isValidUUID, isValidPassword, isEmail] = validators
+    const [requiredRule, isValidUUID, isValidPassword, isEmail, isValidDate] =
+      validators
 
     expect(builderValidator).toBeInstanceOf(BuilderValidator)
-    expect(validators).toHaveLength(4)
+    expect(validators).toHaveLength(5)
     expect(requiredRule).toBeInstanceOf(RequiredRule)
     expect(isValidUUID).toBeInstanceOf(IsValidUUIDRule)
     expect(isValidPassword).toBeInstanceOf(IsValidPasswordRule)
     expect(isEmail).toBeInstanceOf(IsEmailRule)
+    expect(isValidDate).toBeInstanceOf(IsValidDateRule)
   })
 })
